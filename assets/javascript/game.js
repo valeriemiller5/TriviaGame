3a31a6208cd76477e7d200ce66aa9762de0d39db
vflores <valerie.flores64@gmail.com>
2018-07-22 21:31:57 -0700

    added images and style.css for visual improvement to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
-var timer = 10;
+var timer = 45;
+var clock;
 var rightAnswer = 0;
 var wrongAnswer = 0;
 var missingAnswer = 0;
@@ -12,13 +13,13 @@ var retakeQuiz = $("#retakeQuiz").hide();
 
 $("#startQuiz").on("click", takeQuiz);
 
-function countDown(seconds) {
+function countDown() {
     $("#timer").text(timer + " secs");
     if(timer <= 0) {
         results()
     } else {
       timer = timer - 1;
-      setTimeout(countDown, 1000);
+      clock = setTimeout(countDown, 1000);
     }
 };
 
@@ -27,14 +28,16 @@ function takeQuiz() {
       countDown();
       $("#takeQuiz").show();
       $("#gameTimer").show();
+      $("#startQuiz").hide();
 };
 
 document.getElementById("quizForm").onsubmit = function() {
     event.preventDefault();
-    clearTimeout(timer);
+    clearTimeout(clock);
     results();
 };
 
+// this function is only checking the first question, I want it to check all questions for correct, incorrect and unanswered questions...
 function results() {
     if ($("input[name='Question']:checked").val() == 1) {
         rightAnswer = rightAnswer + 1;
@@ -44,17 +47,31 @@ function results() {
         missingAnswer = missingAnswer + 1;
     }
     $(".finalResults").show();
+    $("#retakeQuiz").show();
     $("#takeQuiz").hide();
     $("#correctAnswers").html("You answered " + rightAnswer + " questions correctly.");
     $("#incorrectAnswers").html("You answered " + wrongAnswer + " questions incorrectly.");
     $("#unanswered").html("You left " + missingAnswer + " questions unanswered.");
 };
 
-// once "start quiz" is clicked, timer begins; counts down from 30 secs
-// check if question was answered correctly, incorrectly, or not at all
-// player can only select one answer per question.    
-// allows user to move on to the next question; allowed whether or not the question is answered.
-// if all questions have been answered, show results;
+function reset() {
+    timer = 45;
+    rightAnswer = 0;
+    wrongAnswer = 0;
+    missingAnswer = 0;
+    finalResults = $(".finalResults").hide();
+    gameTimer = $("#gameTimer").hide();
+    startGame = $("#takeQuiz").hide();
+    retakeQuiz = $("#retakeQuiz").hide();
+    $("#startQuiz").show();
+};
+
+$("#retakeQuiz").on("click", reset);
+
+// once "start quiz" is clicked, timer begins; counts down from 45 secs
+// questions may be answered or remain unanswered - everthing is documented once "submit" is clicked
+// run function to check if questions were answered correctly, incorrectly, or not at all
+// if all questions have been answered, show results.  Countdown stops if there is still time on the clock.
 // else, if timer runs out, show results;
 // once "retake quiz" is clicked, timer and quiz start over;
 });
