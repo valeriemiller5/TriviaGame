c041c3caf24606e5deaded25bdbfb2f2c051cb72
vflores <valerie.flores64@gmail.com>
2018-07-23 20:53:12 -0700

    updated Javascript to calculate results better; completed README file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # TriviaGame
+This is a simple trivia quiz where players chose an answer by clicking a radio button next to their selected answer.  The quiz will end when either the "Submit" button is clicked or the timer runs out.  Results will be calculated based on correct, incorrect, and unanswered questions.  Players may retake the quiz by clicking the "Retake Quiz" button on the results page.
+
+# Getting Started
+In order to take the quiz, players must got to the following link:
+https://valeriemiller5.github.io/TriviaGame/
+
+# Prerequisites
+Access to a web browser, such as Google Chrome, is required to take the quiz.
+
+# Built With
+jQuery
+GitHub - repository used for this program
+Contributing
+Please read CONTRIBUTING.md for details on our code of conduct, and the process for submitting pull requests to us.
+
+# Versioning
+For the versions available, see the tags on this repository.
+
+# Authors
+Valerie Flores - Initial work
+
+# License
+This project is licensed under the MIT License - see the LICENSE.md file for details
+
+# Acknowledgments
+Thank you for the tips from class mates, instructor, and those who have posted hints and solutions online.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,7 @@ var retakeQuiz = $("#retakeQuiz").hide();
 
 $("#startQuiz").on("click", takeQuiz);
 
+// once "#startQuiz" is clicked, timer begins; counts down from 45 secs, shows results if timer runs out.
 function countDown() {
     $("#timer").text(timer + " secs");
     if(timer <= 0) {
@@ -31,24 +32,111 @@ function takeQuiz() {
       $("#startQuiz").hide();
 };
 
+// questions may be answered or remain unanswered - everthing is documented once "submit" is clicked
+// Click "submit", show results.  Countdown stops if there is still time on the clock.
 document.getElementById("quizForm").onsubmit = function() {
     event.preventDefault();
     clearTimeout(clock);
     results();
 };
 
-// this function is only checking the first question, I want it to check all questions for correct, incorrect and unanswered questions...
+// this gets the result I want (for each question, checks if it's correct, incorrect, or unanswered), but there
+// must be a simpler way to without so much repetition.
 function results() {
-    $("input:radio").each(function(){
-        var name = $(this).attr("name");
-        if($("input:radio[name="+name+"]:checked").val() == 1){
-            rightAnswer++;
-        } else if($("input:radio[name="+name+"]:checked").val() == 0){
-            wrongAnswer++;
-        } else {
-            missingAnswer++;
-        }
-    });
+    // run function to check if questions were answered correctly, incorrectly, or not at all
+    var questOne = $('input:radio[name="oneQuestion"]:checked').val();
+    var questTwo = $('input:radio[name="twoQuestion"]:checked').val();
+    var questThree = $('input:radio[name="threeQuestion"]:checked').val();
+    var questFour = $('input:radio[name="fourQuestion"]:checked').val();
+    var questFive = $('input:radio[name="fiveQuestion"]:checked').val();
+    var questSix = $('input:radio[name="sixQuestion"]:checked').val();
+    var questSeven = $('input:radio[name="sevenQuestion"]:checked').val();
+    var questEight = $('input:radio[name="eightQuestion"]:checked').val();
+    var questNine = $('input:radio[name="nineQuestion"]:checked').val();
+    var questTen = $('input:radio[name="tenQuestion"]:checked').val();
+    if(questOne == undefined) {
+        missingAnswer++;
+    } else if(questOne == 1) {
+        rightAnswer++;
+    } else {
+        wrongAnswer++;
+    }
+    if(questTwo == undefined) {
+        missingAnswer++;
+    } else if(questTwo == 1) {
+        rightAnswer++;
+    } else {
+        wrongAnswer++;
+    }
+    if(questThree == undefined) {
+        missingAnswer++;
+    } else if(questThree == 1) {
+        rightAnswer++;
+    } else {
+        wrongAnswer++;
+    }
+    if(questFour == undefined) {
+        missingAnswer++;
+    } else if(questFour == 1) {
+        rightAnswer++;
+    } else {
+        wrongAnswer++;
+    }
+    if(questFive == undefined) {
+        missingAnswer++;
+    } else if(questFive == 1) {
+        rightAnswer++;
+    } else {
+        wrongAnswer++;
+    }
+    if(questSix == undefined) {
+        missingAnswer++;
+    } else if(questSix == 1) {
+        rightAnswer++;
+    } else {
+        wrongAnswer++;
+    }
+    if(questSeven == undefined) {
+        missingAnswer++;
+    } else if(questSeven == 1) {
+        rightAnswer++;
+    } else {
+        wrongAnswer++;
+    }
+    if(questEight == undefined) {
+        missingAnswer++;
+    } else if(questEight == 1) {
+        rightAnswer++;
+    } else {
+        wrongAnswer++;
+    }
+    if(questNine == undefined) {
+        missingAnswer++;
+    } else if(questNine == 1) {
+        rightAnswer++;
+    } else {
+        wrongAnswer++;
+    }
+    if(questTen == undefined) {
+        missingAnswer++;
+    } else if(questTen == 1) {
+        rightAnswer++;
+    } else {
+        wrongAnswer++;
+    }
+    // I tried this code that cut out the repetition, but it counted ALL of the radio buttons instead of one
+    // result for each question.
+    //$("input:radio").each(function(){
+    //    var name = $("input:radio").attr("name");
+    //    var quizQuest = $("input:radio[name=" + name + "]:checked").val();
+    //    if(quizQuest == undefined) {
+    //        missingAnswer++;
+    //    } else if (quizQuest == 1) {
+    //        rightAnswer++;
+    //    } else {
+    //        wrongAnswer++;
+    //    }
+    //});
     $(".finalResults").show();
     $("#retakeQuiz").show();
     $("#takeQuiz").hide();
@@ -57,6 +145,7 @@ function results() {
     $("#unanswered").html("You left " + missingAnswer + " questions unanswered.");
 };
 
+// once "#retakeQuiz" is clicked, timer and quiz start over;
 function reset() {
     timer = 45;
     rightAnswer = 0;
@@ -70,13 +159,6 @@ function reset() {
 };
 
 $("#retakeQuiz").on("click", reset);
-
-// once "start quiz" is clicked, timer begins; counts down from 45 secs
-// questions may be answered or remain unanswered - everthing is documented once "submit" is clicked
-// run function to check if questions were answered correctly, incorrectly, or not at all
-// if "submit", show results.  Countdown stops if there is still time on the clock.
-// else, if timer runs out, show results;
-// once "retake quiz" is clicked, timer and quiz start over;
 });
 
   
